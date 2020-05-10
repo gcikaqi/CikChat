f536d535f8521a792df7ed1e41e339c9eb13be14
Gezim Cikaqi <gezim.cikaqi@gmail.com>
2020-05-10 17:52:54 +0200

    sending new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,15 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+// addd a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+})
+
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'shaun');
