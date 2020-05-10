6d5b6f18852e48bcd13dd4b46f9fb09e1f2df2f3
Gezim Cikaqi <gezim.cikaqi@gmail.com>
2020-05-10 18:21:51 +0200

    updating the room

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMsg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 // addd a new chat
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -24,6 +25,15 @@ newNameForm.addEventListener('submit', e => {
     updateMsg.innerText = `Your name was updated to ${newName}`;
     setTimeout(() => updateMsg.innerText = '', 3000);
 })
+
+// update the chat room
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
 
 // check local storage for a name
 const username = localStorage.username ? localStorage.username : 'anon';
