991bbb44f8e981710d58eb30ab09c99abc06d4bd
Gezim Cikaqi <gezim.cikaqi@gmail.com>
2020-05-10 18:38:59 +0200

    bug fixes

--- scripts/app.js
+++ scripts/app.js
@@ -2,45 +2,46 @@
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
-const updateMsg = document.querySelector('.update-mssg');
+const updateMssg = document.querySelector('.update-mssg');
 const rooms = document.querySelector('.chat-rooms');
-// addd a new chat
+
+// add a new chat
 newChatForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const message = newChatForm.message.value.trim();
-    chatroom.addChat(message)
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
     .then(() => newChatForm.reset())
     .catch(err => console.log(err));
 });
 
-// update username
-newNameForm.addEventListener('submit', e => { 
-    e.preventDefault();
-    // update name via chatroom class
-    const newName = newNameForm.name.value.trim();
-    chatroom.updateName(newName);
-    // reset the form
-    newNameForm.reset();
-    // show then hide the update message
-    updateMsg.innerText = `Your name was updated to ${newName}`;
-    setTimeout(() => updateMsg.innerText = '', 3000);
-})
+// update the username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
+});
 
 // update the chat room
 rooms.addEventListener('click', e => {
-    if(e.target.tagName === 'BUTTON'){
-        chatUI.clear();
-        chatroom.updateRoom(e.target.getAttribute('id'));
-        chatroom.getChats(chat => chatUI.render(chat));
-    }
+  if(e.target.tagName === 'BUTTON'){
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
 });
 
-// check local storage for a name
+// check local storage for name
 const username = localStorage.username ? localStorage.username : 'anon';
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', username);
+const chatroom = new Chatroom('gaming', username);
 
-// get chats and render
+// get chats & render
 chatroom.getChats(data => chatUI.render(data));
